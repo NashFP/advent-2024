be0b108e43203b639c8183bf00e038fb99e25f90
Mark Wutka <mark@wutka.com>
2024-12-10 10:31:45 -0500

    mark: added day 10

--- markwutka+ocaml/lib/mwlib.ml
+++ markwutka+ocaml/lib/mwlib.ml
@@ -45,3 +45,37 @@ let string_to_list s = List.of_seq (String.to_seq s)
 let rec gcd a b =
   let m = a mod b in
   if m == 0 then b else gcd b m
+
+let double_cons h l2 l1 = (h :: l1) :: l2
+
+let rec choose n l =
+  match n with
+  | 0 -> []
+  | 1 -> List.map (fun x -> [x]) l
+  | n -> if List.is_empty l then [] else
+           let choose_rest = choose n (List.tl l) in
+           let choose_n1 = choose (n-1) (List.tl l) in
+           let h = List.hd l in
+           List.fold_left (double_cons h) choose_rest choose_n1;;
+
+let rec choose_pairs l =
+  let cons_pair h acc t = (pair h t) :: acc in
+  if List.is_empty l then [] else
+    let choose_rest = choose_pairs (List.tl l) in
+    let h = List.hd l in
+    List.fold_left (cons_pair h) choose_rest (List.tl l)
+
+let rec permute l =
+  let rec loop l pre acc =
+    match l with
+    | [] -> acc
+    | x :: rest ->
+       let permute_rest = permute (List.rev_append pre rest) in
+                   loop rest (x :: pre) (List.fold_left (double_cons x) acc permute_rest)
+  in
+  match l with
+  | [] -> []
+  | [x] -> [[x]]
+  | _ -> loop l [] []
+     
+let digit_to_int ch = Char.code ch - Char.code '0'
