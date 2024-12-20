6c2d35da4c7ae6997ba412af663daf799937c7fe
Mark Wutka <mark@wutka.com>
2024-12-20 13:52:30 -0500

    mark: day 20 - more comments

--- markwutka+ocaml/bin/day20.ml
+++ markwutka+ocaml/bin/day20.ml
@@ -1,13 +1,13 @@
 (*
-  There's not much trickery to this. I originally kepy
-  keep an array whose elements were either 0 meaning the
+  There's not much trickery to this. I originally kept
+  an array whose elements were either 0 meaning the
   cell isn't on the path, or the cell's distance along
   the path. Then I realized that since the path never
   intersects itself, I can figure out the next item
   on the path by keeping track of the previous position
   and making sure I don't go backwards. That allowed
   me to store the distance along the path in the path
-  itself.
+  itself and not have to have a grid structure at all.
 
   When looking for the cheats, a cheat can actually
   cross an existing part of the path, so you can just
@@ -28,6 +28,9 @@ let find_start lines =
 let make_path lines =
   let (sx,sy) = find_start lines in
   let find_next (x,y) (prev_x,prev_y) =
+    (* An x,y is a valid next step if it is a . or a E on the input
+       grid and it isn't the previous x,y (the one before the
+       x,y that this one is the next of*)
     let is_next (x,y) =
       let ch = lines.(y).[x] in
       (ch == '.' || ch == 'E') && (x != prev_x || y != prev_y) in
@@ -39,22 +42,36 @@ let make_path lines =
     else (x,y+1)
   in
   let rec loop pos prev_pos path len =
+    (* Find the next item in the path *)
     let (x,y) = find_next pos prev_pos in
+
+    (* Add it to the path *)
     let next_path = (x,y,len) :: path in
+
     if lines.(y).[x] == 'E' then
+    (* If this item is the E, we are done, reverse the path
+       since we were building it by adding new items to the front. *)
       List.rev next_path
     else
+      (* Otherwise, go find the next position along the path *)
       loop (x,y) pos next_path (len+1)
   in
+  (* Start at the start position with an invalid prev pos.
+     We go ahead and add the start to the path list with a length
+     of 1, and set the next len to 2 *)
   loop (sx,sy) (-1,-1) [(sx,sy,1)] 2
 
 let cheat_list max_pico min_cheat path =
+  (* Since the cheat paths can cross both walls and existing paths,
+     we can just use Manhattan dist for the distance (|x0-x1| + |y0-y1|). *)
   let grid_dist (x0,y0) (x1,y1) =
     abs (x0-x1) + abs (y0-y1) in
   let try_add_cheats (fx,fy,curr_val) cheats (dx,dy,cheat_val) =
     let dist = grid_dist (fx,fy) (dx,dy) in
-    if dist <= max_pico && cheat_val > curr_val + dist &&
-         (cheat_val - (curr_val + dist)) >= min_cheat then
+    (* Make sure the distance is within the allowable cheat distance
+       and that amount of savings exceeds the minimum amount we
+       are looking for. *)
+    if dist <= max_pico && (cheat_val - (curr_val + dist)) >= min_cheat then
       cheats + 1
     else
       cheats   
@@ -63,6 +80,9 @@ let cheat_list max_pico min_cheat path =
     match curr_path with
     | [] -> cheats
     | from_pos :: rest ->
+       (* To find the cheats, we take the head item from the path
+          and compute all the possible cheats from it to the rest
+          of the path. *)
        loop rest (List.fold_left (try_add_cheats from_pos) cheats rest)
   in
   loop path 0
