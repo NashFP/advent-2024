2da8ca92055e59aa516351ff7c98e3cca28b30ca
Mark Wutka <mark@wutka.com>
2024-12-20 13:41:35 -0500

    mark: day 20 optimization - eliminate grid

--- markwutka+ocaml/bin/day20.ml
+++ markwutka+ocaml/bin/day20.ml
@@ -1,7 +1,13 @@
 (*
-  There's not much trickery to this. I keep an array
-  whose elements are either 0 meaning the cell isn't
-  on the path, or the cell's distance along the path.
+  There's not much trickery to this. I originally kepy
+  keep an array whose elements were either 0 meaning the
+  cell isn't on the path, or the cell's distance along
+  the path. Then I realized that since the path never
+  intersects itself, I can figure out the next item
+  on the path by keeping track of the previous position
+  and making sure I don't go backwards. That allowed
+  me to store the distance along the path in the path
+  itself.
 
   When looking for the cheats, a cheat can actually
   cross an existing part of the path, so you can just
@@ -19,39 +25,34 @@ let find_start lines =
              (Mwlib.product (Mwlib.range 0 width)
                 (Mwlib.range 0 height)))
 
-let make_grid lines =
-  let width = String.length lines.(0) in
-  let height = Array.length lines in
+let make_path lines =
   let (sx,sy) = find_start lines in
-  let array_init x y = if x == sx && y == sy then 1 else 0 in
-  let new_grid = Array.init_matrix width height array_init in  
-  let find_next (x,y) =
+  let find_next (x,y) (prev_x,prev_y) =
     let is_next (x,y) =
-      (lines.(y).[x] == '.' || lines.(y).[x] == 'E') &&
-        new_grid.(x).(y) == 0 in
+      let ch = lines.(y).[x] in
+      (ch == '.' || ch == 'E') && (x != prev_x || y != prev_y) in
+    
+    (* Try each direction, only one should succeed *)
     if is_next (x-1,y) then (x-1,y)
     else if is_next (x,y-1) then (x,y-1)
     else if is_next (x+1,y) then (x+1,y)
     else (x,y+1)
   in
-  let rec loop pos path len =
-    let (x,y) = find_next pos in
-    new_grid.(x).(y) <- len;
-    let next_path = (x,y) :: path in
+  let rec loop pos prev_pos path len =
+    let (x,y) = find_next pos prev_pos in
+    let next_path = (x,y,len) :: path in
     if lines.(y).[x] == 'E' then
-      (List.rev next_path, new_grid)
+      List.rev next_path
     else
-      loop (x,y) next_path (len+1)
+      loop (x,y) pos next_path (len+1)
   in
-  loop (sx,sy) [(sx,sy)] 2
+  loop (sx,sy) (-1,-1) [(sx,sy,1)] 2
 
-let cheat_list max_pico min_cheat grid path =
+let cheat_list max_pico min_cheat path =
   let grid_dist (x0,y0) (x1,y1) =
     abs (x0-x1) + abs (y0-y1) in
-  let try_add_cheats (fx,fy) cheats (dx,dy) =
+  let try_add_cheats (fx,fy,curr_val) cheats (dx,dy,cheat_val) =
     let dist = grid_dist (fx,fy) (dx,dy) in
-    let cheat_val = grid.(dx).(dy) in
-    let curr_val = grid.(fx).(fy) in 
     if dist <= max_pico && cheat_val > curr_val + dist &&
          (cheat_val - (curr_val + dist)) >= min_cheat then
       cheats + 1
@@ -69,9 +70,9 @@ let cheat_list max_pico min_cheat grid path =
 
 let day20 () =
   let lines = Array.of_list (Mwlib.read_file "data/day20.txt") in
-  let (path,grid) = make_grid lines in
-  let resulta = cheat_list 2 100 grid path in
-  let resultb = cheat_list 20 100 grid path in
+  let path = make_path lines in
+  let resulta = cheat_list 2 100 path in
+  let resultb = cheat_list 20 100 path in
   Printf.printf "day20a = %d\nday20b = %d\n" resulta resultb;;
   
 day20 ();;
